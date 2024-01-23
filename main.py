8c0be0ef35df19b02ddfdb84d9ade9ecc6bbe0dd
cstlore <ok15022000@gmail.com>
2024-01-23 19:40:07 +0400

    1

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-import pygame
+import pygame as pg
+from random import randrange
+import pymunk.pygame_util
 
-background_colour = (37, 150, 190)
-screen = pygame.display.set_mode((1000, 600))
-pygame.display.set_caption('gmfdjgfgfdg')
-screen.fill(background_colour)
-pygame.display.flip()
-running = True
+pymunk.pygame_util.positive_y_is_up = False
+background_colour = (156, 229, 255)
+RES = WIDTH, HEIGHT = 1000, 600
+FPS = 60
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+pg.init()
+surface = pg.display.set_mode(RES)
+clock = pg.time.Clock()
+draw_options = pymunk.pygame_util.DrawOptions(surface)
+
+space = pymunk.Space()
+space.gravity = 0, 8000
+segment_shape = pymunk.Segment(space.static_body, (1, HEIGHT), (WIDTH, HEIGHT), 26)
+space.add(segment_shape)
+segment_shape.elasticity = 0.4
+segment_shape.friction = 1.0
+
+while True:
+    surface.fill(background_colour)
+
+    for i in pg.event.get():
+        if i.type == pg.QUIT:
+            exit()
+
+    space.step(1 / FPS)
+    space.debug_draw(draw_options)
+
+    pg.display.flip()
+    clock.tick(FPS)
