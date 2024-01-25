a3e59eb1a192b8336dc3e51f7f8e2254044b9c95
cstlore <ok15022000@gmail.com>
2024-01-25 21:06:49 +0400

    Update

--- Level.py
+++ Level.py
@@ -18,14 +18,11 @@ class Level:
     def run_level(self):
         # Инициализация Pygame
         pg.init()
-        # Определение цветов
-        black = (0, 0, 0)
-        yellow = (255, 255, 0)
-        red = (255, 0, 0)
         # Установка размеров окна
-        window_width = 1000
-        window_height = 600
-        window = pg.display.set_mode((window_width, window_height))
+        self.window_width = 1000
+        self.window_height = 600
+        self.FPS = 60
+        window = pg.display.set_mode((self.window_width, self.window_height))
         window.fill((245, 234, 233))
         # Отображение изображения на всем окне
         background = load_image('bricks.png')
@@ -33,23 +30,31 @@ class Level:
         background.set_alpha(100)
         window.blit(background, (0, 0))
         pg.display.flip()
-        self.generate_blocks()
-        # Создание объекта шрифта
-        font = pg.font.Font(None, 104)
+        # физика
+
+        clock = pg.time.Clock()
+        draw_options = pymunk.pygame_util.DrawOptions(window)
+
+        # настройки Pymunk
+        self.space = pymunk.Space()
+        self.space.gravity = 0, 8000
 
         # Основной цикл программы
         running1 = True
+        self.generate_blocks()
         while running1:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running1 = False
                     pg.quit()
                     sys.exit()
-
+            self.space.step(1 / self.FPS)
+            self.space.debug_draw(draw_options)
             # Обновление экрана
             self.all_sprites.draw(window)
             self.all_sprites.update()
             pg.display.flip()
+            clock.tick(self.FPS)
 
     def generate_blocks(self):
         if self.type == 1:
@@ -65,6 +70,17 @@ class Level:
             Thorn(self.all_sprites, 480, 280)
             Thorn(self.all_sprites, 440, 280)
             Thorn(self.all_sprites, 520, 280)
+            # платформа
+            segment_shape = pymunk.Segment(self.space.static_body, (2, self.window_height),
+                                           (self.window_width, self.window_height), 120)
+            self.space.add(segment_shape)
+            segment_shape.elasticity = 0.8
+            segment_shape.friction = 1.0
+            platform = pymunk.Segment(self.space.static_body, (420, 340),
+                                      (580, 340), 20)
+            self.space.add(platform)
+            platform.elasticity = 0.8
+            platform.friction = 1.0
         if self.type == 2:
             pass
         if self.type == 3:
